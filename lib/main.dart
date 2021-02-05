e829b5af32a12f4ff69af0751fef667c3d688c1d
Bayu Indra Kusuma <bayuindrakusuma05@gmail.com>
2021-02-05 15:51:42 +0700

    Task1

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:byblog/sign_up.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Blog Bayuik',
+      theme: ThemeData.light(),
+      home: SignUp(),
+    );
+  }
+}
